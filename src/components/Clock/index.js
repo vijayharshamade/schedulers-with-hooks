7805584b7f2e7d6cf292c422f3c74af39fc8d24a
vijayharshamade <vharsha736.vh@gmail.com>
2022-11-05 09:51:20 +0000

    schedulers with hooks

---
+++ src/components/Clock/index.js
@@ -0,0 +1,31 @@
+import {useState, useEffect} from 'react'
+
+import {ClockContainer, Heading, ClockImage, Time} from './styledComponents'
+
+const Clock = () => {
+  const [date, setDate] = useState(new Date())
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setDate(new Date())
+    }, 1000)
+    console.log('effect executed and interval is scheduled')
+    return () => {
+      clearInterval(timerId)
+      console.log('clean up function is called')
+    }
+  }, []) // if dependency array is removed we will see multiple logs in the console. to avoid that we pass dependency array
+
+  return (
+    <ClockContainer>
+      <Heading>Clock</Heading>
+      <ClockImage
+        src="https://assets.ccbp.in/frontend/hooks/clock-img.png"
+        alt="clock"
+      />
+      <Time>{date.toLocaleTimeString()}</Time>
+    </ClockContainer>
+  )
+}
+
+export default Clock
